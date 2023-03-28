bec3070252c466e4a26d64b6b48fa8c1b84f85ef
Alzter <alexsmudgy20@gmail.com>
2023-03-28 13:14:06 +1100

    fog background wip

--- SourceCode/scenes/backgrounds/assets/AlignBackgroundToScreenBottom.gd
+++ SourceCode/scenes/backgrounds/assets/AlignBackgroundToScreenBottom.gd
@@ -5,8 +5,10 @@ extends ParallaxBackground
 
 func _ready():
 	ResolutionManager.connect("window_resized", self, "window_resized")
+	Global.connect("player_loaded", self, "window_resized")
 
 func window_resized():
 	var offset = ResolutionManager.window_resolution.y - 480
+	var camera = Global.get_current_camera()
 	offset = max(offset, 0)
 	scroll_base_offset.y = offset
