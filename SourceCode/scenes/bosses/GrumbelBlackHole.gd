2034ed8995f754059162ef79434e5ac40c711332
Alzter <alexsmudgy20@gmail.com>
2023-04-06 14:26:39 +1000

    black hole now makes player airborne

--- SourceCode/scenes/bosses/GrumbelBlackHole.gd
+++ SourceCode/scenes/bosses/GrumbelBlackHole.gd
@@ -2,13 +2,15 @@ extends Area2D
 
 onready var hitbox = $CollisionShape2D
 onready var size = hitbox.shape.radius
+#onready var kill_hitbox = $KillHitbox
 
-export var pull_strength = Vector2(100, 300)
-export var resist_pull_strength = Vector2(1, 100)
+export var pull_strength = Vector2(200, 300)
+export var resist_pull_strength = Vector2(2, 100)
 
 func _process(delta):
 	for body in get_overlapping_bodies():
 		if body.is_in_group("players"):
+			body.jump_terminable = false
 			var distance = global_position - body.global_position
 			var pull_factor = distance.length() / size
 			pull_factor = max(1 - pull_factor, 0)
@@ -24,6 +26,11 @@ func _process(delta):
 			#body.position += pull_velocity
 			body.velocity += pull_velocity
 			body.grounded = false
+	
+#	for body in kill_hitbox.get_overlapping_bodies():
+#		if body.is_in_group("players"):
+#			if !body.invincible:
+#				body.die()
 
 func set_size(new_size):
 	hitbox.shape.set_deferred("radius", new_size)
