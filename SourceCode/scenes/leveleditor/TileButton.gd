edf87dbbc7ac6b5229668aa3f7e4f61fab1c63bf
Alzter <alexsmudgy20@gmail.com>
2023-04-29 10:32:55 +1000

    Tile buttons now show large tiles

--- SourceCode/scenes/leveleditor/TileButton.gd
+++ SourceCode/scenes/leveleditor/TileButton.gd
@@ -15,12 +15,19 @@ func update_current_tile_id(new_value):
 	
 	var tile_texture = tileset.tile_get_texture(tile_id)
 	var tile_texture_size = tileset.tile_get_region(tile_id).size
+	print(tileset.tile_get_name(tile_id), tile_texture_size)
 	sprite.set_texture(tile_texture)
-	print(tile_texture_size)
 	
 	if tileset.tile_get_tile_mode(tile_id) == 1:
 		sprite.region_rect.position = tileset.autotile_get_icon_coordinate(tile_id) * Global.TILE_SIZE
-	else: sprite.region_rect.position = tileset.tile_get_region(tile_id).position
+	else:
+		sprite.region_rect = tileset.tile_get_region(tile_id)
+		
+		# Scale down the tile sprite if the tile is larger than 32x32
+		var rect_size = max(sprite.region_rect.size.x, sprite.region_rect.size.y)
+		sprite.scale = Vector2.ONE * (Global.TILE_SIZE / rect_size)
+	
+	
 
 func _on_TileButton_pressed():
 	emit_signal("tile_button_pressed", self, tile_id)
