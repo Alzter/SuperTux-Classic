864f7a5bf08acd8920add163574d0495209fe68b
Alzter <alexsmudgy20@gmail.com>
2023-05-01 22:50:32 +1000

    Make lava hitboxes invisible

--- SourceCode/scenes/objects/LavaHitbox.gd
+++ SourceCode/scenes/objects/LavaHitbox.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+onready var sprite = $Sprite
+
+func _ready():
+	sprite.hide()
+
 func _on_Lava_body_entered(body):
 	# If the body is invincible, don't kill it
 #	if body.get("invincible"):
