d6bd1beee4cfc1249b32f16756b01845706d59bb
Alzter <alexsmudgy20@gmail.com>
2023-05-04 22:50:35 +1000

    Open World menu functionality 2

--- SourceCode/scenes/leveleditor/menus/MainMenu.gd
+++ SourceCode/scenes/leveleditor/menus/MainMenu.gd
@@ -2,6 +2,7 @@ extends Control
 
 onready var menu_main = $MainMenu
 onready var menu_create_world = $CreateWorldMenu
+onready var menu_open_world = $OpenWorldMenu
 
 onready var button_world_create = $MainMenu/VBoxContainer/WorldCreate
 onready var button_world_open = $MainMenu/VBoxContainer/WorldOpen
@@ -16,6 +17,11 @@ func _ready():
 func _on_WorldCreate_pressed():
 	menu_main.hide()
 	menu_create_world.popup()
+
+func _on_WorldOpen_pressed():
+	menu_main.hide()
+	menu_open_world.popup()
+
 
 
 func _on_ExitToMenu_pressed():
@@ -33,7 +39,10 @@ func _on_WorldOpen_mouse_entered():
 func _on_ExitToMenu_mouse_entered():
 	button_exit.grab_focus()
 
-
 func _on_CreateWorldMenu_popup_hide():
 	menu_main.show()
 	button_world_create.grab_focus()
+
+func _on_OpenWorldMenu_popup_hide():
+	menu_main.show()
+	button_world_open.grab_focus()
