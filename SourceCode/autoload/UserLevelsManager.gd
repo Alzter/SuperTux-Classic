4f40126dbc7e191807bbddf7f9fcc31c7b7a80fc
Alzter <alexsmudgy20@gmail.com>
2023-05-04 23:14:04 +1000

    Load User Worlds Function

--- SourceCode/autoload/UserLevelsManager.gd
+++ SourceCode/autoload/UserLevelsManager.gd
@@ -1,16 +1,32 @@
 extends Node
 
+var user_worlds_data: Dictionary = {}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func load_user_worlds():
+	var dir = Directory.new()
+
+	dir.open("user://")
+	if !dir.dir_exists("worlds"):
+		dir.make_dir("worlds")
+	else:
+		dir.change_dir("worlds")
+		dir.list_dir_begin(true, true)
+		while true:
+			var current: String = dir.get_next()
+			if current == "":
+				break
+			else:
+				register_user_world(current)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func register_user_world(dir_name: String) -> bool:
+	var dir = Directory.new()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# If worlds pack data exists for the current user world:
+	if dir.dir_exists("user://worlds/" + dir_name) and dir.file_exists("user://worlds/" + dir_name + "/world.data"):
+		var file = ConfigFile.new()
+		file.load("user://worlds/" + dir_name + "/world.data")
+		user_worlds_data[dir_name] = file
+		return true
+	
+	return false
