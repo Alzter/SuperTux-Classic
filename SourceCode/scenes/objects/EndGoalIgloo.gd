65889d7dc6dd26e9c262d7344a84a45cdd91437f
Alzter <alexsmudgy20@gmail.com>
2023-06-06 12:52:06 +1000

    Fix editor igloo bug

--- SourceCode/scenes/objects/EndGoalIgloo.gd
+++ SourceCode/scenes/objects/EndGoalIgloo.gd
@@ -19,4 +19,5 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("players"):
-		body.state_machine.set_state("win_inside_igloo")
+		if body.state_machine.state == "win":
+			body.state_machine.set_state("win_inside_igloo")
