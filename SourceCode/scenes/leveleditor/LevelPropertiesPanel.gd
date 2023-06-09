71a0564c75c99706184673ac198d924c6ba31a24
Alzter <alexsmudgy20@gmail.com>
2023-06-09 14:33:34 +1000

    WIP "Add Custom Music" function in level properties

--- SourceCode/scenes/leveleditor/LevelPropertiesPanel.gd
+++ SourceCode/scenes/leveleditor/LevelPropertiesPanel.gd
@@ -10,7 +10,8 @@ onready var level_time = level_properties.get_node("EditLevelTimer/Time")
 onready var level_gravity = level_properties.get_node("EditLevelGravity/Gravity")
 onready var level_autoscroll_speed = level_properties.get_node("EditLevelAutoscroll/AutoscrollSpeed")
 
-var music_tracks : Dictionary = {}
+var music_tracks := {}
+var custom_music_files := {}
 
 var level = null
 
@@ -32,22 +33,59 @@ func _update_music_list():
 	music_tracks = {}
 	level_music.clear()
 	
+	# -------------------------------------------------------------------------
+	# "Add custom track..." button
+	var plus_icon = preload("res://images/editor/icons/add_small.png")
+	
+	level_music.add_icon_item(plus_icon, "Add custom track...", id)
+	music_tracks["Custom"] = id
+	id += 1
+	
+	#level_music.add_separator()
+	
+	# -------------------------------------------------------------------------
+	
 	# Load custom music tracks
+	custom_music_files = {}
+	
 	var custom_tracks = UserLevels.get_custom_music_tracks_for_world()
 	if custom_tracks:
-		for song in custom_tracks:
-			level_music.add_item(song, id)
-			music_tracks[song] = id
+		for song_file in custom_tracks:
+			
+			var custom_song_extension = song_file.get_extension()
+			var custom_song_name = song_file.get_file().trim_suffix("." + custom_song_extension)
+			
+			custom_music_files[custom_song_name] = song_file
+			
+			level_music.add_item(custom_song_name, id)
+			music_tracks[custom_song_name] = id
 			id += 1
+		
+		#level_music.add_separator()
 	
-	# Load all base-game music tracks
-	for song in Music.songs:
+	# -------------------------------------------------------------------------
+	
+	# Load all base-game music tracks (add in alphabetical order)
+	var songs = Music.songs
+	songs.sort()
+	
+	for song in songs:
+		print(str(song) + ", " + str(id))
 		level_music.add_item(song, id)
 		music_tracks[song] = id
 		id += 1
 
 # WHY GODOT?! WHY MUST I JUMP THROUGH SUCH HOOPS?!
 func _set_music_to_song(song_name : String):
+	
+	# Translate a custom song filepath from URL to string format
+	var id := 0
+	for custom_song_file in custom_music_files.values():
+		if custom_song_file == song_name:
+			var custom_song_name = custom_music_files.keys()[id]
+			song_name = custom_song_name
+		id += 1
+	
 	if music_tracks.has(song_name):
 		var song_id = music_tracks.get(song_name)
 		level_music.select(song_id)
@@ -62,11 +100,26 @@ func _on_Author_text_changed(new_text):
 		level.level_author = new_text
 		Global.clear_level_cache(UserLevels.current_level)
 
+# SET LEVEL MUSIC TO ITEM SELECTED ON SONG SELECTOR
 func _on_Music_item_selected(index):
-	if level:
+	# "Add custom song" button
+	if index == 0:
+		_set_music_to_song(level.music)
+		print("Add custom song...")
+		
+	elif level:
+		
+		# Set music to selected song.
 		for song_id in music_tracks.values():
 			if song_id == index:
 				var song_name = music_tracks.keys()[song_id]
+				
+				# If song is custom music track,
+				# actually set the level music to the filepath (URL)
+				# of the custom music track.
+				if custom_music_files.has(song_name):
+					song_name = custom_music_files.get(song_name)
+				
 				level.music = song_name
 
 func _on_TimerEnabled_toggled(button_pressed):
