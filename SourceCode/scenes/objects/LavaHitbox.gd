908a36e94b334879022cd267bf3ed0e311f3b149
Alzter <alexsmudgy20@gmail.com>
2023-06-10 15:47:59 +1000

    Fix Lava being really laggy

--- SourceCode/scenes/objects/LavaHitbox.gd
+++ SourceCode/scenes/objects/LavaHitbox.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-onready var sprite = $Sprite
+onready var sprite = get_node_or_null("Sprite")
 
 func _ready():
-	sprite.hide()
+	if sprite: sprite.hide()
 
 func _on_Lava_body_entered(body):
 	# If the body is invincible, don't kill it
